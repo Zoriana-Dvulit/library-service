e21696a46e6b098d6ab1502960225b398355a8e5
Zoriana Dvulit <zdvulit@ukr.net>
2023-04-24 18:05:53 +0300

    Added serializers.py in borrowing & user apps, modified serializers.py in books app

--- books/serializers.py
+++ books/serializers.py
@@ -1,7 +1,6 @@
-from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
-from books.models import Book, Borrowing
+from books.models import Book
 
 
 class BookSerializer(serializers.ModelSerializer):
@@ -15,40 +14,3 @@ class BookSerializer(serializers.ModelSerializer):
             "inventory",
             "daily_fee"
         ]
-
-
-class BorrowingSerializer(serializers.ModelSerializer):
-    book = BookSerializer()
-
-    class Meta:
-        model = Borrowing
-        fields = [
-            "id",
-            "borrow_date",
-            "expected_return_date",
-            "actual_return_date",
-            "book"
-        ]
-
-    def validate_book(self, value):
-        if value.inventory == 0:
-            raise serializers.ValidationError("Book is not available for borrowing.")
-        return value
-
-    def create(self, validated_data):
-        book = validated_data["book"]
-        book.inventory -= 1
-        book.save()
-        borrowing = Borrowing.objects.create(
-            book=book,
-            borrower=self.context["request"].user,
-        )
-
-
-User = get_user_model()
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = "__all__"
---
+++ borrowing/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+
+from books.serializers import BookSerializer
+from borrowing.models import Borrowing
+
+
+class BorrowingSerializer(serializers.ModelSerializer):
+    book = BookSerializer()
+
+    class Meta:
+        model = Borrowing
+        fields = [
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book"
+        ]
+
+    def validate_book(self, value):
+        if value.inventory == 0:
+            raise serializers.ValidationError("Book is not available for borrowing.")
+        return value
+
+    def create(self, validated_data):
+        book = validated_data["book"]
+        book.inventory -= 1
+        book.save()
+        borrowing = Borrowing.objects.create(
+            book=book,
+            borrower=self.context["request"].user,
+        )
