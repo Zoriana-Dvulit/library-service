2606af8fa30eec2bf74946aa5b951975a4dd4a4b
Zoriana Dvulit <zdvulit@ukr.net>
2023-04-24 14:16:33 +0300

    Updated README.md & urls.py for formatting code & optimizing imports in every module

--- library/urls.py
+++ library/urls.py
@@ -18,9 +18,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("books.urls", namespace="books")),
-    path("__debug__/", include("debug_toolbar.urls")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+                  path("admin/", admin.site.urls),
+                  path("", include("books.urls", namespace="books")),
+                  path("__debug__/", include("debug_toolbar.urls")),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
