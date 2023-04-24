9492faa6bd7a3d813f0d72369a9f4ddd92f32fef
Zoriana Dvulit <zdvulit@ukr.net>
2023-04-24 17:57:32 +0300

    Added "borrowing" & "user" in INSTALLED_APPS, changed AUTH_USER_MODEL to "user.Customer"

--- library/settings.py
+++ library/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY = "django-insecure-6vubhk2$++agnctay_4pxy_8cq)mosmn(*-#2b^v4cgsh-^!i3"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
     "debug_toolbar",
     "books",
     "library",
+    "borrowing",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -113,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = "books.Customer"
+AUTH_USER_MODEL = "user.Customer"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
