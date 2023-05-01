c4d3cd6de84407c3ecd1304b92761cac03cd2cd1
Zoriana Dvulit <zdvulit@ukr.net>
2023-05-01 17:53:36 +0300

    Imported HttpResponse in borrowing.views.py

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseBadRequest
+from django.http import HttpResponseBadRequest, HttpResponse
 from django.shortcuts import get_object_or_404
 from django.utils import timezone
 from django.views import View
