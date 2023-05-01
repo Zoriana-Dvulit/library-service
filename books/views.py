18f9a8b0bf45192282c14add1b70045620186a66
Zoriana Dvulit <zdvulit@ukr.net>
2023-05-01 17:39:50 +0300

    Created permission.py file with moved  class IsAdminOrReadOnly from books.views

--- books/views.py
+++ books/views.py
@@ -1,16 +1,9 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.permissions import BasePermission, SAFE_METHODS
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from books.models import Book
+from books.permission import IsAdminOrReadOnly
 from books.serializers import BookSerializer
-
-
-class IsAdminOrReadOnly(BasePermission):
-    def has_permission(self, request, view):
-        if request.method in SAFE_METHODS:
-            return True
-        return request.user.is_authenticated and request.user.is_staff
 
 
 class BookList(ListCreateAPIView):
