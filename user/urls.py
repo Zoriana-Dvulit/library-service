0862d46a161a031cdde5d797bfb5195851c6c67c
Zoriana Dvulit <zdvulit@ukr.net>
2023-05-21 13:33:51 +0300

    Updated urls.py & views.py for User

--- user/urls.py
+++ user/urls.py
@@ -1,49 +1,16 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from user.views import UserViewSet
 
-from user.views import (
-    UserCreateView,
-    UserRetrieveView,
-    UserUpdateView,
-    UserDeleteView
-)
+router = DefaultRouter()
+router.register("", UserViewSet, basename="user")
 
 urlpatterns = [
-    path(
-        "api/token/",
-        TokenObtainPairView.as_view(),
-        name="token_obtain_pair"
-    ),
-    path(
-        "api/token/refresh/",
-        TokenRefreshView.as_view(),
-        name="token_refresh"
-    ),
-    path(
-        "api/token/verify/",
-        TokenVerifyView.as_view(),
-        name="token_verify"
-    ),
-    path(
-        "",
-        UserCreateView.as_view(),
-        name="user-create"
-    ),
-    path(
-        "<int:pk>/",
-        UserRetrieveView.as_view(),
-        name="user-retrieve"
-    ),
-    path(
-        "<int:pk>/update/",
-        UserUpdateView.as_view(),
-        name="user-update"
-    ),
-    path(
-        "<int:pk>/delete/",
-        UserDeleteView.as_view(),
-        name="user-delete"
-    ),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("", include(router.urls)),
 ]
 
 app_name = "user"
--- user/views.py
+++ user/views.py
@@ -1,28 +1,12 @@
 from django.contrib.auth import get_user_model
-from rest_framework import generics, permissions
+from rest_framework import viewsets, permissions
 
 from user.serializers import UserSerializer
 
 User = get_user_model()
 
 
-class UserCreateView(generics.CreateAPIView):
-    serializer_class = UserSerializer
-
-
-class UserRetrieveView(generics.RetrieveAPIView):
+class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
-
-
-class UserUpdateView(generics.UpdateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-
-class UserDeleteView(generics.DestroyAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAdminUser]
