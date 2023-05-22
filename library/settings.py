7715c6b2498b1d5c84c4c6c40ec16e8d94925355
Zoriana Dvulit <zdvulit@ukr.net>
2023-05-22 12:12:00 +0300

    Added installation & configuration of spectacular package for generating API documentation

--- library/settings.py
+++ library/settings.py
@@ -153,6 +153,11 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 
 }
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Library Service",
+    "DESCRIPTION": "Library Service description",
+}
+
 
 SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": ("Authorize",),
--- library/urls.py
+++ library/urls.py
@@ -44,5 +44,20 @@ urlpatterns = [
                       TokenVerifyView.as_view(),
                       name="token_verify"
                   ),
+                  path(
+                      "api/schema/",
+                      SpectacularAPIView.as_view(),
+                      name="schema"
+                  ),
+                  path(
+                      "api/schema/swagger-ui/",
+                      SpectacularSwaggerView.as_view(url_name="schema"),
+                      name="swagger-ui"
+                  ),
+                  path(
+                      "api/schema/redoc/",
+                      SpectacularRedocView.as_view(url_name="schema"),
+                      name="redoc"
+                  ),
 
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
