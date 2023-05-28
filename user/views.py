729cf68601fce07bf57e4573642d142aefdd4f1e
Zoriana Dvulit <zdvulit@ukr.net>
2023-05-28 16:22:20 +0300

    Updated user/views.py with UserViewSet & UserRegistrationSerializer

--- user/views.py
+++ user/views.py
@@ -1,10 +1,10 @@
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets, permissions, status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from user.serializers import UserSerializer
+from user.serializers import UserSerializer, UserRegistrationSerializer
 
 User = get_user_model()
 
@@ -19,48 +19,44 @@ class UserViewSet(viewsets.ModelViewSet):
             return []
         return super().get_permissions()
 
-
-@api_view(["POST"])
-def register_user(request):
-    """
-    Endpoint: /register/
-    Method: POST
-    Expected Input (Request Body):
-    {
-        "username": string,
-        "email": string,
-        "password": string
-    }
-    Expected Output (Response):
-    {
-        "message": string,
-        "user": {
-            "id": integer,
+    @action(methods=["POST"], detail=False)
+    def register_user(self, request):
+        """
+        Endpoint: /user/register/
+        Method: POST
+        Expected Input (Request Body):
+        {
             "username": string,
             "email": string,
-            ...
-        },
-        "access_token": string,
-        "refresh_token": string
-    }
-    """
-    username = request.data.get("username")
-    email = request.data.get("email")
-    password = request.data.get("password")
+            "password": string
+        }
+        Expected Output (Response):
+        {
+            "message": string,
+            "user": {
+                "id": integer,
+                "username": string,
+                "email": string,
+                ...
+            },
+            "access_token": string,
+            "refresh_token": string
+        }
+        """
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            access_token = str(refresh.access_token)
+            refresh_token = str(refresh)
 
-    user = User.objects.create_user(username=username, email=email, password=password)
+            response_data = {
+                "message": "User registered successfully",
+                "user": UserSerializer(user).data,
+                "access_token": access_token,
+                "refresh_token": refresh_token
+            }
 
-    serializer = UserSerializer(user)
-
-    refresh = RefreshToken.for_user(user)
-    access_token = str(refresh.access_token)
-    refresh_token = str(refresh)
-
-    response_data = {
-        "message": "User registered successfully",
-        "user": serializer.data,
-        "access_token": access_token,
-        "refresh_token": refresh_token
-    }
-
-    return Response(response_data, status=status.HTTP_201_CREATED)
+            return Response(response_data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
