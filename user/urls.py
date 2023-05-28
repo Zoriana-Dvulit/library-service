f8498293aec55147080edf16a144970a8b654a03
Zoriana Dvulit <zdvulit@ukr.net>
2023-05-28 14:55:58 +0300

    Added registration endpoint

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
-from user.views import UserViewSet
+from user.views import UserViewSet, register_user
 
 router = DefaultRouter()
 router.register("", UserViewSet, basename="user")
@@ -10,6 +10,7 @@ urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("register/", register_user, name="register"),
     path("", include(router.urls)),
 ]
 
