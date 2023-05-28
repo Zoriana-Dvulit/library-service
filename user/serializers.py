bb73ba089de423d4471f0a23f4ba32af3388b640
Zoriana Dvulit <zdvulit@ukr.net>
2023-05-28 16:21:02 +0300

    Added UserRegistrationSerializer to user.serializer.py

--- user/serializers.py
+++ user/serializers.py
@@ -8,3 +8,12 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
+
+
+class UserRegistrationSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=150)
+    email = serializers.EmailField()
+    password = serializers.CharField(max_length=128)
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
